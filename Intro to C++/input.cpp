378d0eb3073213f0a6530dc169e5cfdebadfe5cf
Summet Parab <sumeetparab4113@gmail.com>
2024-05-02 13:06:38 +0530

    moved input.cpp

---
+++ Intro to C++/input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    //int age;
+   // float age;
+    //double age;
+    //char age;
+
+    //compiler accepts input for bool datatype only in the form of 0&1
+    // and not true or false
+    bool age;
+    cout << "Enter your age : \n" ;
+    cin >> age;
+    cout << "Your Age is : " << age << "\n";
+    return 0;
+ }
--- Intro to C++/variables&Datatypes.cpp
+++ Intro to C++/variables&Datatypes.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 
 int main(){
+    // int marks = 50;
+    // cout << "Sumeet's Marks =  " << marks << "\n" ;
+
+    // int
     int marks = 50;
-    cout << "Sumeet's Marks =  " << marks << "\n" ;
+    // float
+    float weight = 88.4444444;
+    //double
+    double vajan = 88.4444444;
+    //char
+    char alphabet = 'S';
+    //boolean
+    bool isMale = true;
+    bool isFemale = false;
+    bool isBoy = 1;
+    bool isGirl = 0;
+
+    cout << marks << "\n";
+    cout << weight << "\n";
+    cout << vajan << "\n";
+    cout << alphabet << "\n";
+    cout << isMale << "\n";
+    cout << isFemale << "\n";
+    cout << isBoy << "\n";
+    cout << isGirl << "\n\n";
+
+    cout << sizeof(marks) << "\n";
+    cout << sizeof(weight) << "\n";
+    cout << sizeof(vajan) << "\n";
+    cout << sizeof(alphabet) << "\n";
+    cout << sizeof(isMale) << "\n";
+    cout << sizeof(isFemale) << "\n";
+    cout << sizeof(isBoy) << "\n";
+    cout << sizeof(isGirl) << "\n";
 }
