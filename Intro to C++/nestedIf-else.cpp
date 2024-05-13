d40c6ae884bc98d065ead9486a43dfe9d12dfae2
Summet Parab <sumeetparab4113@gmail.com>
2024-05-13 14:27:03 +0530

    nested if-else

---
+++ Intro to C++/nestedIf-else.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float height, weight;
+    cout << "Enter your HEIGhT in FEET : ";
+    cin >> height;
+    
+
+    if(height > 5.7){
+        cout << "Enter your  WEIGHT in KG : ";
+        cin >> weight;
+        if(weight > 65){
+            cout << "Your BMI is very good!!";
+        }
+        else{
+            cout  << "Bhai tuze polio hai kya...kitna kam weight hai tera.";
+        }
+    }
+    else{
+        cout << "Are ye baccha kiska khogaya. Bhai pehle tu COMPLAN pi aur height badha fir baat karenge tuzse";
+    }
+    return 0;
+}
