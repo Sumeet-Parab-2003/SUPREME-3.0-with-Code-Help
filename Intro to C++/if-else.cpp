984fd9ad92a792d917054b1f613c95f928f9cab5
Summet Parab <sumeetparab4113@gmail.com>
2024-05-13 12:06:14 +0530

    if-else statements

--- Intro to C++/if-else.cpp
+++ Intro to C++/if-else.cpp
@@ -2,15 +2,28 @@
 using namespace std;
 
 int main(){
-    int budget ;
-    cout << "Enter ur budget : ";
-    cin >> budget ;
+    // int budget ;
+    // cout << "Enter ur budget : ";
+    // cin >> budget ;
     
-    if(budget > 1500000){
-        cout << "You can buy XUV 700!!";
+    // if(budget > 1500000){
+    //     cout << "You can buy XUV 700!!";
+    // }
+    // else{
+    //     cout << "Bhai teri aukaat waali gaadi dekhle ;]";
+    // }
+
+
+    int age;
+    cout << "Enter your age : ";
+    cin >> age;
+
+    if(age > 18){
+        cout << "You are an Adult and you are eligible to vote, drive vehicles, drink, etc";
     }
     else{
-        cout << "Bhai teri aukaat waali gaadi dekhle ;]";
+         cout << "You are still a minor, Bhai Bada hoja pehle. ";
+
     }
 
 }
