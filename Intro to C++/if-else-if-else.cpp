461914a0ee14515e958fda78b060e159cb5b6e70
Summet Parab <sumeetparab4113@gmail.com>
2024-05-13 14:13:21 +0530

    if-else-if-else

---
+++ Intro to C++/if-else-if-else.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks;
+    cout << "Enter Your makrs : ";
+    cin >> marks;
+    
+    if(marks > 90){
+        cout << "Well done! You got A+ grade.";
+    }
+    else if(marks > 80){
+        cout << "Well done! You got A grade.";
+    }
+    else if(marks > 70){
+        cout << "Great! You got B grade.";
+    }
+    else if(marks > 60){
+        cout << "You got C grade.";
+    }
+    else if(marks > 50){
+        cout << "You got D grade.";
+    }
+    else if(marks > 40){
+        cout << "You got E grade.";
+    }
+    else{
+        cout << "Your garde is F i.e you are FAILED :(";
+    }
+
+    return 0;
+}
