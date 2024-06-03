b2a43fccd99a471f93a5ff8a1b24d6844a1f5cc9
Summet Parab <sumeetparab4113@gmail.com>
2024-06-03 16:53:45 +0530

    Intro to For loop

--- Intro to C++/Loops/For/intro_to_for_loop.cpp
+++ Intro to C++/Loops/For/intro_to_for_loop.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    //Printin My name 10 times
-    for(int i=0; i<10; i++){
-        cout << "Sumeet" << endl;
+    //Printing My name 10 times
+    // for(int i=0; i<10; i++){
+    //     cout << "Sumeet" << endl;
+    // }
+
+    //Printing no.s from 1 to 100
+    for (int i = 1; i <= 100; i++){
+        cout << i << " " ;
     }
 
     return 0;
