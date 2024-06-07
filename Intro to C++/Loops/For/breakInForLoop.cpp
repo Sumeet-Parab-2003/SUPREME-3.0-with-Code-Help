be6db9c37c284e9ce6f427641c05aaa265ddabd9
Summet Parab <sumeetparab4113@gmail.com>
2024-06-07 13:44:38 +0530

    Changes

--- Intro to C++/Loops/For/breakInForLoop.cpp
+++ Intro to C++/Loops/For/breakInForLoop.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 
-    // print 1 to 5 no.s with condition in loop ---> i=1 ; i<=10 ; i++
+    // print 1 to 5 no.s without changing condition in loop ---> i=1 ; i<=10 ; i++
     for (int i = 1; i<=10; i++){
         cout << i << " ";
         if(i==5){
