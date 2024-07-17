bee4b1631cbc521cb5cccb36ab64cfa1a2a11851
Summet Parab <sumeetparab4113@gmail.com>
2024-07-17 15:28:11 +0530

    Do-while Intro

---
+++ Intro to C++/Loops/Do-while/Intro.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // for(int i=1; i<=5; i++){
+    //     cout << i << " ";
+    // }
+    // cout << endl;
+
+    //initialization
+    // int i = 1;
+    // do{
+    //     //logic
+    //     cout << i << " ";
+    //     //updation
+    //     i++;
+    // }while(i<=5);  //condition
+
+    //EXERCISE
+    
+    // for(int i; i<=10; i++);{
+    //     cout << i;
+    // }
+
+    // int i;
+    // if(cin >> i){
+    //     cout << "Sumeet";
+    // }
+
+    if(cout << "Hiiii"){
+        cout << "Sumeet";
+    }
+
+
+
+    return 0;
+}
